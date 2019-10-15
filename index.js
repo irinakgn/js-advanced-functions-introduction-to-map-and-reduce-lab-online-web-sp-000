449287ef8129a8cc68541bd87030b482d492eded
Irina Kagan <irinagamb@gmail.com>
2019-10-15 00:05:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const  mapToSquare =(array) => array.map(m => m * m)
 
 
 
-function reduceToTotal(array, startingPoint = 96){
+function reduceToTotal(array, startingPoint = 100){
   return array.reduce((a, c)=>{
     a = a + c
     return a
