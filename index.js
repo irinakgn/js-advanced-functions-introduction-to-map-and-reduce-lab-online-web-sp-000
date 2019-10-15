d5bbfded07fbbcf7088378d8bc68e1fc8be7986f
Irina Kagan <irinagamb@gmail.com>
2019-10-15 00:03:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+const mapToNegativize = (array) => {
+  return array.map(m=> m *-1)
+}
+
+const mapToNoChange = (array) => {
+  return array.map(m=> m)
+}
+
+
+const  mapToDouble =(array) => array.map(m => m * 2)
+const  mapToSquare =(array) => array.map(m => m * m)
+
+// mapToSquare(sourceArray)
+
+const reduceToTotal = (array, startingPoint) => array.reduce((a, c)=>{
+  a = a + c
+  console.log(c)
+  return a
+}, startingPoint)
